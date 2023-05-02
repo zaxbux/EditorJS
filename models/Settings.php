a4ba1096a943768e60568d2680c90ae5ec2aa56a
Zachary Schneider <hello@zacharyschneider.ca>
2023-05-01 18:55:39 -0700

    fix: detect installed plugins and enable integrations

--- models/Settings.php
+++ models/Settings.php
@@ -3,6 +3,7 @@
 namespace ReaZzon\Editor\Models;
 
 use Model;
+use System\Classes\PluginManager;
 
 /**
  * Class Settings
@@ -24,4 +25,21 @@ class Settings extends Model
      * @var string Reference to field configuration
      */
     public $settingsFields = 'fields.yaml';
+
+    /**
+     * Disables form fields based on presence of plugins.
+     */
+    public function filterFields($fields, $context = null)
+    {
+        $pluginManager = PluginManager::instance();
+
+        $integrations = [
+            'integration_winter_blog' => 'Winter.Blog',
+            'integration_winter_pages' => 'Winter.Pages',
+        ];
+
+        foreach ($integrations as $key => $value) {
+            $fields->{$key}->disabled = !$pluginManager->hasPlugin($value);
+        }
+    }
 }
